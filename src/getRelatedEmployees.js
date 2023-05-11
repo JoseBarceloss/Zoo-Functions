949c752ef7bf74764d9da2f0d93814729c2081d0
Barcelos <barcelosps41710@gmail.com>
2023-05-11 03:12:38 -0300

    exercicio 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const { employees } = data;
+  return employees.some((employee) => employee.managers.includes(id));
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const { employees } = data;
+  const relatedEmployees = employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  return relatedEmployees;
 };
 
 module.exports = { isManager, getRelatedEmployees };
