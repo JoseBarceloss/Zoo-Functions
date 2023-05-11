f9e3fbe6a47d24518922a051c4ade91b1387b6e2
Barcelos <barcelosps41710@gmail.com>
2023-05-11 03:47:55 -0300

    exercicio 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = ({ species = null, sex = null } = {}) => {
+  if (species) {
+    const speciesData = data.species.find((s) => s.name === species);
+    if (!speciesData) return 0;
+
+    if (sex) {
+      const filteredResidents = speciesData.residents.filter((resident) => resident.sex === sex);
+      return filteredResidents.length;
+    }
+
+    return speciesData.residents.length;
+  }
+
+  return data.species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
 };
 
 module.exports = countAnimals;
