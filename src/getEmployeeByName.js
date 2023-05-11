a3e9eaa5b4743946cb54ff52347cdd34deed259b
Barcelos <barcelosps41710@gmail.com>
2023-05-11 01:22:35 -0300

    exercicio 3 completo e passou no lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  const employee = data.employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName);
+
+  if (!employee) {
+    return {};
+  }
+  const { id, firstName, lastName, managers, responsibleFor } = employee;
+
+  return { id, firstName, lastName, managers, responsibleFor };
 };
 
 module.exports = getEmployeeByName;
